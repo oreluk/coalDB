dfd3fce40f0137127f1f8b1b40cf5288b1c3d6fc
Jim Oreluk <jim.oreluk@gmail.com>
2016-05-28 21:56:23 -0700

    export data grabs relevant id's
    stores in struct. need to see what is best for QOI indentification from
    the data.

--- exportButton.m
+++ exportButton.m
@@ -1,5 +1,28 @@
-function exportButton(hh, dd, expID, groupID)
+function expData = exportButton(hh, dd, Htable, data)
+struct expData
+count = 0;
+for i = 1:size(Htable.Data,1)
+    if Htable.Data{i,1} == 1
+        count = 1;
+    end
+end
 
+if count ~= 1
+    errordlg('Requires only one experiment selected for export to B2BDC')
+end
 
+%% Grabs Data of Checked Column
+for i = 1:size(Htable.Data,1)
+    if Htable.Data{i,1} == 1
+        expData.id.speID = data.click{i,2};
+        expData.id.expID = data.click{i,8};
+        expData.id.bibID = data.click{i,9};
+        expData.id.dgID = data.click{i,10};
+        break
+    end
+end
+
+% get pertinent data. 
+keyboard
 
 end
